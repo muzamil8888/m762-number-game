d6bd7c8626f5938ef74645d1531dfc1577f95f3d
muzamil8888 <mmuzammilmeo456@gmail.com>
2024-03-25 18:19:59 -0700

    changed

--- index.ts
+++ index.ts
@@ -7,12 +7,12 @@ const randomNumber = Math.floor(Math.random() *10+ 1);
 
 const answers = await inquirer.prompt([
   {
-    name: "userGuessNumber",
+    name: "userGuessedNumber",
     type: "number",
     message: "please select a number between 10 - 1",
   },
 ]);
-if (answers.userGuessNumber === randomNumber) {
+if (answers.userGuessedNumber === randomNumber) {
   console.log("CONGRATULATION! you guessed correct number");
 } else {
   console.log("opps! you guessed wrong number");
